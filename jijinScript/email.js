27d403e0de77ccca8d034eb04fa888e04d8a3bc7
yogachen <yogachen@tencent.com>
2022-04-21 20:07:21 +0800

    update

--- jijinScript/email.js
+++ jijinScript/email.js
@@ -1,34 +1,35 @@
-var multer  = require('nodemailer');
+var multer = require('nodemailer');
 var mailTransport = multer.createTransport({
-    host : 'smtp.qq.com',
+    host: 'smtp.qq.com',
     secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
-    auth : {
-        user : '1527627156@qq.com',
-        pass : 'wvocdhnuhyjdgjeg'
+    auth: {
+        user: '1527627156@qq.com',
+        pass: 'cwvocdhnuhyjdgjegbh'
     },
 });
-exports.sendEmail=function (arrayTo,content){
-    var addrString="";
+exports.sendEmail = function (arrayTo, content) {
+    var addrString = "";
     for (var i = arrayTo.length - 1; i >= 0; i--) {
-        addrString+=arrayTo[i];
-        if (i!=0) {
-            addrString+=',';
+        addrString += arrayTo[i];
+        if (i != 0) {
+            addrString += ',';
         }
     }
-   var options = {
-        from           : '"chenbinhao" <1527627156@qq.com>',
-        to             : addrString,
-        subject        : '一封来自SingerStone的邮件',
-        html           : '<html><body style="background-color:red;">'+content+'</body></html>'
+    console.log("发送给：addrString");
+    var options = {
+        from: '"chenbinhao" <1527627156@qq.com>',
+        to: addrString,
+        subject: '一封来自SingerStone的邮件',
+        html: '<html><body style="background-color:red;">' + content + '</body></html>'
     };
 
-     mailTransport.sendMail(options, function(err, msg){
-        if(err){
+    mailTransport.sendMail(options, function (err, msg) {
+        if (err) {
             console.log(err);
-         }
+        }
         else {
             console.log("send email success!");
-           
+
         }
     });
 } 
